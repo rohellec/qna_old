23fb11576e33590431914000d8223cb3faa62e06
Dima Rogov <ds.rogv@gmail.com>
2018-04-13 19:25:07 +0300

    Update app to path all new specs

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,7 @@
 class AnswersController < ApplicationController
-  before_action :set_question, only: [:new, :create]
+  before_action :authenticate_user!
+  before_action :set_question,  only: :create
+  before_action :correct_user?, only: :destroy
 
   def new
     @answer = Answer.new
@@ -7,21 +9,36 @@ class AnswersController < ApplicationController
 
   def create
     @answer = @question.answers.build(answer_params)
+    @answer.user = current_user
     if @answer.save
       flash[:success] = "New answer has been successfully created"
       redirect_to @question
     else
-      render :new
+      render "questions/show", hidden: false
     end
+  end
+
+  def destroy
+    @answer.delete
+    flash[:success] = "Answer has been successfully deleted"
+    redirect_to @answer.question
   end
 
   private
 
-  def set_question
-    @question = Question.find(params[:question_id])
-  end
-
   def answer_params
     params.require(:answer).permit(:body)
+  end
+
+  def correct_user?
+    @answer = Answer.find(params[:id])
+    user = @answer.user
+    redirect_to root_path if user != current_user
+  end
+
+  def set_question
+    @question = Question.find(params[:question_id])
+    @user = @question.user
+    @answers = @question.answers.select { |question| question.persisted? }
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def correct_user?(user)
+    user_signed_in? && current_user == user
+  end
 end
