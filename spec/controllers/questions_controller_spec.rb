9457231665ef7426c9aca353ee570523c933eb7a
Dima Rogov <ds.rogv@gmail.com>
2018-04-13 19:23:05 +0300

    Update controllers specs with reliable scenarios

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
 describe QuestionsController do
+  let(:user)     { create(:confirmed_user) }
+  let(:question) { create(:question) }
+
+  before { request.env['devise.mapping'] = Devise.mappings[:user] }
+
   describe "GET #index" do
-    let(:questions) { FactoryBot.create_pair(:question) }
+    let(:questions) { create_pair(:question) }
 
     before { get :index }
 
@@ -16,8 +21,7 @@ describe QuestionsController do
   end
 
   describe "GET #show" do
-    let(:question) { FactoryBot.create(:question) }
-    let(:answers)  { FactoryBot.create_pair(:answer, question: question) }
+    let(:answers)  { create_pair(:answer, question: question) }
 
     before { get :show, params: { id: question } }
 
@@ -26,54 +30,161 @@ describe QuestionsController do
     end
 
     it "populates an array of answers for current question" do
-      expect(assigns(:answers)).to eq(answers)
+      expect(assigns(:answers)).to match_array(answers)
     end
 
     it "renders 'show' view" do
       expect(response).to render_template :show
     end
+
+    context "with new_answer param" do
+      context "as authenticated user" do
+        before do
+          sign_in user
+          get :show, params: { id: question, new_answer: true }
+        end
+
+        it "builds new answer" do
+          expect(assigns(:answer)).to be_a_new Answer
+        end
+
+        it "renders 'show' form" do
+          expect(response).to render_template :show
+        end
+      end
+
+      context "as non-authenticated user" do
+        before { get :show, params: { id: question, new_answer: true } }
+
+        it "redirects to sign in path" do
+          expect(response).to redirect_to new_user_session_path
+        end
+      end
+    end
   end
 
   describe "GET #new" do
-    before { get :new }
+    context "as authenticated user" do
+      before do
+        sign_in user
+        get :new
+      end
 
-    it "builds new question" do
-      expect(assigns(:question)).to be_a_new(Question)
+      it "builds new question" do
+        expect(assigns(:question)).to be_a_new(Question)
+      end
+
+      it "renders 'new' view" do
+        expect(response).to render_template :new
+      end
     end
 
-    it "renders 'new' view" do
-      expect(response).to render_template :new
+    context "as non-authenticated user" do
+      before { get :new }
+
+      it "redirects to sign in path" do
+        expect(response).to redirect_to new_user_session_path
+      end
     end
   end
 
   describe "POST #create" do
-    context "with valid attributes" do
-      let(:valid_attributes) { FactoryBot.attributes_for(:question) }
+    let(:valid_attributes)   { attributes_for(:question, user: user) }
+    let(:invalid_attributes) { attributes_for(:invalid_question) }
 
-      it "saves the new question to db" do
-        expect do
-          post :create, params: { question: valid_attributes }
-        end.to change(Question, :count).by(1)
+    context "as authenticated user" do
+      before do
+        sign_in user
+        get :new
       end
 
-      it "redirects to 'show' view" do
-        post :create, params: { question: valid_attributes }
-        expect(response).to redirect_to question_path(assigns(:question))
+      context "with valid attributes" do
+        it "saves the new question to db" do
+          expect do
+            post :create, params: { question: valid_attributes }
+          end.to change(Question, :count).by(1)
+        end
+
+        it "creates new question for user" do
+          expect do
+            post :create, params: { question: valid_attributes }
+          end.to change(user.questions, :count).by(1)
+        end
+
+        it "redirects to 'show' view" do
+          post :create, params: { question: valid_attributes }
+          expect(response).to redirect_to question_path(assigns(:question))
+        end
+      end
+
+      context "with invalid attributes" do
+        it "doesn't save the new question to db" do
+          expect do
+            post :create, params: { question: invalid_attributes }
+          end.not_to change(Question, :count)
+        end
+
+        it "re-renders 'new' view" do
+          post :create, params: { question: invalid_attributes }
+          expect(response).to render_template :new
+        end
       end
     end
 
-    context "with invalid attributes" do
-      let(:invalid_attributes) { FactoryBot.attributes_for(:invalid_question) }
+    context "as non-authenticated user" do
+      before { post :create, params: { question: valid_attributes } }
 
-      it "doesn't save the new question to db" do
-        expect do
-          post :create, params: { question: invalid_attributes }
-        end.not_to change(Question, :count)
+      it "redirects to sign in path" do
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    context "as authenticated user" do
+      let!(:user_question) { create(:question, user: user) }
+
+      before { sign_in user }
+
+      context "as a question's author" do
+        it "deletes question from db" do
+          expect do
+            delete :destroy, params: { id: user_question }
+          end.to change(Question, :count).by(-1)
+        end
+
+        it "deletes question from user" do
+          expect do
+            delete :destroy, params: { id: user_question }
+          end.to change(user.questions, :count).by(-1)
+        end
+
+        it "redirects to 'index'" do
+          delete :destroy, params: { id: user_question }
+          expect(response).to redirect_to questions_path
+        end
       end
 
-      it "re-renders 'new' view" do
-        post :create, params: { question: invalid_attributes }
-        expect(response).to render_template :new
+      context "other user's question" do
+        let!(:other_user_question) { create(:question) }
+
+        it "doesn't delete question from db" do
+          expect do
+            delete :destroy, params: { id: other_user_question }
+          end.not_to change(Question, :count)
+        end
+
+        it "redirects to root_path" do
+          delete :destroy, params: { id: other_user_question }
+          expect(response).to redirect_to root_path
+        end
+      end
+    end
+
+    context "as non-authenticated user" do
+      it "redirects to sign in path" do
+        delete :destroy, params: { id: question }
+        expect(response).to redirect_to new_user_session_path
       end
     end
   end
