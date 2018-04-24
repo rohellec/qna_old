15e0b3b143088c5d2d8306a6ea6f33a5157a70f0
Dima Rogov <ds.rogv@gmail.com>
2018-04-25 01:17:36 +0300

    Update question using js

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,18 +1,20 @@
-$('.edit-question-link').click(function(e) {
-  e.preventDefault();
+$(document).on('turbolinks:load', function() {
+  $('.edit-question-link').click(function(e) {
+    e.preventDefault();
 
-  var editLink = $(this);
-  var editForm = $('.edit-question');
-  var question = $('.question');
+    var current  = $(this);
+    var editForm = $('.edit-question');
+    var question = $('.question');
 
-  if (!current.hasClass('cancel')) {
-    current.addClass('cancel');
-    current.text('Cancel');
-  } else {
-    current.removeClass('cancel');
-    current.text('Edit');
-  }
+    if (!current.hasClass('cancel')) {
+      current.addClass('cancel');
+      current.text('Cancel');
+    } else {
+      current.removeClass('cancel');
+      current.text('Edit');
+    }
 
-  editForm.toggle();
-  question.toggle();
-}
+    editForm.toggle();
+    question.toggle();
+  });
+});
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,7 @@
 class QuestionsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
-  before_action :set_question,  only: [:show, :destroy]
-  before_action :correct_user?, only: :destroy
+  before_action :set_question,  only: [:show, :edit, :update, :destroy]
+  before_action :correct_user?, only: [:edit, :update, :destroy]
 
   def index
     @questions = Question.all
@@ -16,6 +16,9 @@ class QuestionsController < ApplicationController
     @question = Question.new
   end
 
+  def edit
+  end
+
   def create
     @question = Question.new(question_params)
     @question.user = current_user
@@ -23,6 +26,17 @@ class QuestionsController < ApplicationController
       redirect_to @question, flash: { success: "New question has been successfully created" }
     else
       render :new
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @question.update(question_params)
+        flash.now[:success] = "Question has been successfully updated"
+        format.js { render "update", layout: false }
+      else
+        format.js { render "error_messages", layout: false }
+      end
     end
   end
 
