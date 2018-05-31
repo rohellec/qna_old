32fb1c7522161e8d354e88ecb3412f1aa465c5db
Dima Rogov <ds.rogv@gmail.com>
2018-05-31 19:22:08 +0300

    Add unique acceptnace validation to Answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,4 +5,12 @@ class Answer < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true
+  validate  :unique_acceptance
+
+  def unique_acceptance
+    return unless accepted?
+    accepted_answer = Answer.accepted.where(question: question)
+    accepted_answer = accepted_answer.where('id != ?', id) if persisted?
+    errors.add(:base, "Only one answer can be accepted") if accepted_answer.exists?
+  end
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 describe Answer do
+  let(:user) { create(:user) }
+  let(:question) { create(:question, user: user) }
+
   it { is_expected.to belong_to :user }
   it { is_expected.to belong_to :question }
   it { is_expected.to validate_presence_of :body }
@@ -17,6 +20,20 @@ describe Answer do
 
     it "doesn't include non-accepted answers" do
       expect(Answer.accepted).not_to include answer
+    end
+  end
+
+  describe "validation :unique_acceptance" do
+    let!(:accepted_answer) { create(:accepted_answer, question: question) }
+    let!(:answer) { build(:accepted_answer, question: question) }
+
+    it "doesn't save answer to database if invalid" do
+      expect { answer.save }.not_to change(question.answers, :count)
+    end
+
+    it "adds error message to invalid answer" do
+      answer.save
+      expect(answer.errors.full_messages).to include "Only one answer can be accepted"
     end
   end
 end
