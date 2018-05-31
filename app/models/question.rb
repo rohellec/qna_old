9d490025155f300e253d4a5e1bac461354841be5
Dima Rogov <ds.rogv@gmail.com>
2018-05-31 19:01:33 +0300

    Add ordering for question's answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
-  has_many   :answers, dependent: :destroy
+  has_many   :answers, -> { order({ accepted: :desc, created_at: :asc }) },
+             dependent: :destroy
 
   validates :title, :body, presence: true
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,9 +1,29 @@
 require 'rails_helper'
 
 describe Question do
+  let(:question) { create(:question) }
+
   it { is_expected.to belong_to(:user) }
   it { is_expected.to have_many(:answers).dependent(:destroy) }
 
   it { is_expected.to validate_presence_of :title }
   it { is_expected.to validate_presence_of :body  }
+
+  describe "#answers" do
+    let!(:answer_one) { create(:answer, question: question) }
+    let!(:answer_two) { create(:answer, question: question) }
+
+    context "when there is no accepted answer" do
+      it "ordered by :created_at" do
+        expect(question.answers).to eq [answer_one, answer_two]
+      end
+    end
+
+    context "when there is an accepted answer" do
+      it "ordered by :accepted" do
+        answer_two.accept
+        expect(question.answers).to eq [answer_two, answer_one]
+      end
+    end
+  end
 end
