16990206d0c9c88890a2228a39c600808f97e6e9
Dima Rogov <ds.rogv@gmail.com>
2018-07-20 00:29:03 +0300

    Configure CarrierWave for dev and test envs

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,4 +40,24 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Configure CarrierWave for test environment
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = false
+  end
+end
+
+# Separate CarrierWave uploader folder for test environment
+CarrierWave::Uploader::Base.descendants.each do |klass|
+  next if klass.anonymous?
+  klass.class_eval do
+    def cache_dir
+      "#{Rails.root}/spec/support/uploads/tmp"
+    end
+
+    def store_dir
+      "#{Rails.root}/spec/support/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
+  end
 end
