cdee009ae9e741e237d34e5fc93d2aea47c8cd08
Dima Rogov <ds.rogv@gmail.com>
2018-09-09 23:13:31 +0300

    Add :user_vote_ability validation to vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -6,4 +6,11 @@ class Vote < ApplicationRecord
   belongs_to :votable, polymorphic: true
 
   validates :user_id, uniqueness: { scope: :votable_type }
+  validate  :user_vote_ability
+
+  private
+
+  def user_vote_ability
+    errors.add(:user, "can't vote on his own question") if user == votable.user
+  end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -8,6 +8,22 @@ describe Vote do
   # should use this hack
   it do
     subject.user = create(:user)
+    subject.votable = create(:question)
     is_expected.to validate_uniqueness_of(:user_id).scoped_to(:votable_type)
+  end
+
+  describe "validation :user_vote_ability" do
+    let!(:user)    { create(:user) }
+    let!(:votable) { create(:question, user: user) }
+    let!(:vote)    { build(:up_vote, votable: votable, user: user) }
+
+    it "doesn't save vote in db if it's invalid" do
+      expect { vote.save }.not_to change(votable.votes, :count)
+    end
+
+    it "adds error message to invalid vote" do
+      vote.valid?
+      expect(vote.errors.full_messages).to include "User can't vote on his own question"
+    end
   end
 end
