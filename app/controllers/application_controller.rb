ae71ec56e14c9145cd0b66cb912159d3033a78ea
Dima Rogov <ds.rogv@gmail.com>
2019-01-29 11:05:49 +0300

    Add Serialized module to controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,14 +15,4 @@ class ApplicationController < ActionController::Base
   def resource_type
     controller_name.singularize
   end
-
-  def respond_unprocessable_with_message(message)
-    respond_to do |format|
-      format.json { render json: message, status: :unprocessable_entity }
-      format.html do
-        flash[:notice] = message
-        redirect_back(fallback_location: root_url)
-      end
-    end
-  end
 end
---
+++ app/controllers/concerns/serialized.rb
@@ -0,0 +1,28 @@
+module Serialized
+  extend ActiveSupport::Concern
+
+  def check_resource_found
+    item = instance_variable_get "@#{resource_type}"
+    render_not_found unless item
+  end
+
+  def render_json_with_message(item = nil, message = nil)
+    item    ||= instance_variable_get "@#{resource_type}"
+    message ||= t(".message")
+    render json: Hash[resource_type, item, :message, message]
+  end
+
+  def render_not_found
+    render plain: t("http_error.not_found"), status: :not_found
+  end
+
+  def render_errors(item = nil)
+    item ||= instance_variable_get "@#{resource_type}"
+    render json: item.errors.full_messages, status: :forbidden
+  end
+
+  def render_unprocessable_with_message(message = nil)
+    message ||= t("http_error.unprocessable_entity")
+    render plain: message, status: :unprocessable_entity
+  end
+end
